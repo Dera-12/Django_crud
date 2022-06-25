f855653b1e84a4b2e2c4d8bb2920c41e7ba6c6c1
chidera Jessica <jescachidera@gmail.com>
2022-06-25 03:45:13 +0100

    initial commit

---
+++ zuri14GOO9864AGW/zuri14GOO9864AGW/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for zuri14GOO9864AGW project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zuri14GOO9864AGW.settings')
+
+application = get_asgi_application()
